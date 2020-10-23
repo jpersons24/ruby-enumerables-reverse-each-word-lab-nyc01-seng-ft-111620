0d62ba089bd51c41b3f1c8531435c2fc6d630cb7
Jakob Persons <jakobep24@gmail.com>
2020-10-23 15:45:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,13 @@ string_of_words = "Hello there, how are you?"
 
 def reverse_each_word(string)
   string_array = string.split
-  reversed_string_array = []
+  # reversed_string_array = []
 
-  string_array.each do |word|
-    reversed_string_array << word.reverse
+  string_array.collect do |word|
+    word.reverse
   end
-  p reversed_string_array.join(" ")
+  p string_array.join(" ")
+  # p reversed_string_array.join(" ")
 end
 
 reverse_each_word(string_of_words)
