8ae8f8399ec0e208a0b13424fb3d4117d615dc9a
Jakob Persons <jakobep24@gmail.com>
2020-10-23 15:35:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,7 @@ def reverse_each_word(string)
   string_array.each do |word|
     reversed_string_array = []
     reversed_string_array << word.reverse
-    p reversed_string_array.join
+    puts reversed_string_array.join
   end
 end
 
